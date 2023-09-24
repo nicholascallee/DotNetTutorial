16bbf1b88295e8301ec08ba1010ee6235d892b34
nicholascallee <nicholascallee@gmail.com>
2023-09-24 11:04:57 -0500

    created category repository class and interfaces.

---
+++ Bulky.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+
+        void Save();
+    }
+}
