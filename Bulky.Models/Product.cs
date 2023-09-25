382d753dafddb41f435416db00b33e4c930fb810
nicholascallee <nicholascallee@gmail.com>
2023-09-25 13:25:42 -0500

    added Image Url to product

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -43,5 +43,7 @@ namespace BulkyBook.Models
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
