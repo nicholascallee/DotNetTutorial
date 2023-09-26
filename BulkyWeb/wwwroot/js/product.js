a4b5cf6eece30429a4a354ce0388584f107d4090
nicholascallee <nicholascallee@gmail.com>
2023-09-26 16:05:12 -0500

    using datatables with edit button added

---
+++ BulkyWeb/wwwroot/js/product.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            { data: 'title', "width": "25%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'listPrice', "width": "10%" },
+            { data: 'author', "width": "15%" },
+            { data: 'category.name', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                        <a href="" class="btn btn-primary mx-2">
+                            <i class="bi bi-pencil-square"></i> Edit</a>          
+                    </div>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
