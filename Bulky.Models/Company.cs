dcd791ae8fa9a46980bda18a5db6f4b0d190ff04
nicholascallee <nicholascallee@gmail.com>
2023-09-28 13:09:07 -0500

    Working on company crud operations

---
+++ Bulky.Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace BulkyBook.Models
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? StreetAddress { get; set; }
+        public String? City { get; set; }
+        public String? State { get; set; }
+        public String? PostalCode { get; set; }
+        public String? PhoneNumber { get; set; }
+    }
+}
