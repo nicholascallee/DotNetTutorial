a9b4ae5539040bcd7da266178f1c2fbcd2364cb7
nicholascallee <nicholascallee@gmail.com>
2023-10-02 11:46:45 -0500

    finished functionality for order management

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -19,5 +19,20 @@ namespace BulkyBook.Utility
         public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
         public const string PaymentStatusRejected = "Rejected";
 
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "Processing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+
+        public const string SessionCart = "SessionShoppingCart";
+
+
+
+
+
     }
 }
