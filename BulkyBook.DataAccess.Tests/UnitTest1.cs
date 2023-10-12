d6e4ac2e64a8fe946f8d51143a04d68c47fc1e5a
nicholascallee <nicholascallee@gmail.com>
2023-10-11 21:50:13 -0500

    Data Access Test Solution created

---
+++ BulkyBook.DataAccess.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BulkyBook.DataAccess.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
